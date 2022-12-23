54effc7d5b1406b4556cc10e299f35e3f5956573
MobileAppDev451 <113200931+MobileAppDev451@users.noreply.github.com>
2022-12-23 02:45:21 +0100

    Version 1.0.4.5
    Custom searchbar bugfix

--- app/src/main/java/de/biSlaveNumberOne/exposedrealfun/Main.kt
+++ app/src/main/java/de/biSlaveNumberOne/exposedrealfun/Main.kt
@@ -346,7 +346,7 @@ class Main : AppCompatActivity() {
 
 
                 //JavaScript/CSS injection mobile header
-                val cssHeader = "html{-webkit-tap-highlight-color: transparent;}.erf-homepage-pagination{overflow: auto;}.d-flex { overflow: auto; } /*custom-search-bar*/ .erf-search { position: fixed; left: 0; top: 0; right: 0; background-color: #ffffff; z-index: 99999; padding: 20px; box-shadow: 1px 1px 11px 2px #000000a8; } /*custom-search-bar end*/" //your css as String
+                val cssHeader = "html{-webkit-tap-highlight-color: transparent;}.erf-homepage-pagination{overflow: auto;}.d-flex { overflow: auto; } .filter-dropmenu{overflow: unset;} /*custom-search-bar*/ .erf-search { position: fixed; left: 0; top: 0; right: 0; background-color: #ffffff; z-index: 99999; padding: 20px; box-shadow: 1px 1px 11px 2px #000000a8; } /*custom-search-bar end*/" //your css as String
                 val jsHeader = "var style = document.createElement('style'); style.innerHTML = '$cssHeader'; " +
                         "document.getElementsByTagName('nav')[0].style.display = 'none';" +
                         "document.head.appendChild(style);"
@@ -372,6 +372,8 @@ class Main : AppCompatActivity() {
                     },
                     1000 // value in milliseconds
                 )
+                searchState = false
+
                 super.onPageFinished(webView, url)
             }
 
